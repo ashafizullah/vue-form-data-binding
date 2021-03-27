78b00cb2573085c2cb63e8f6ee8cec77d753b4b0
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2021-03-27 22:05:24 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignupFrom/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupFrom from './components/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignupFrom
   }
 }
 </script>
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
 }
 </style>
